c7767488825d042ce03b54286a5e1020f48e0bd4
Sachin-0099 <itssachin0099@gmail.com>
2025-01-02 23:03:06 +0530

    Services added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ContactUs from './components/ContactUs'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './components/Login'
+import Services from './pages/Services'
+import About from './pages/About'
 
 const App = () => {
   return (
@@ -10,6 +12,9 @@ const App = () => {
       <Route path='/' element={<Home/>}/>
       <Route path='/contact' element={<ContactUs/>}/>
       <Route path='/Login' element={<Login/>}/>
+      <Route path='/About' element={<About/>}/>
+
+      <Route path='/Services' element={<Services/>}/>
     </Routes>
   )
 }
