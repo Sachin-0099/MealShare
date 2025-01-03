4704b874f8aab9a03314aa1858057be13ecd4919
Bharat Intern Web Development Project <itsprince544@gmail.com>
2025-01-04 01:08:15 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import ContactUs from './components/ContactUs'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './components/Login'
-<<<<<<< Updated upstream
 import Services from './pages/Services'
 import About from './pages/About'
-=======
+
 import Footer from './components/Footer'
 
->>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -17,13 +15,13 @@ const App = () => {
       <Route path='/' element={<Home/>}/>
       <Route path='/contact' element={<ContactUs/>}/>
       <Route path='/Login' element={<Login/>}/>
-<<<<<<< Updated upstream
+
       <Route path='/About' element={<About/>}/>
 
       <Route path='/Services' element={<Services/>}/>
-=======
+
       <Route path='/Footer' element={<Footer/>}/>
->>>>>>> Stashed changes
+
     </Routes>
   )
 }
