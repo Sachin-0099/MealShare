f82a45092bdf1b80c891e2d8f6e130fffe964383
Dwivedi Priya <pd7668665@gmail.com>
2025-01-04 23:27:05 +0530

    Routing Improved

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,38 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+const NavData=[
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "About",
+    path: "/About",
+  },
+  {
+    label: "Services",
+    path: "/Services",
+  },
+  {
+    label: "Donation",
+    path: "/Donation",
+  },
+  {
+    label: "How It Works",
+    path: "/Works",
+  }
+]
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center px-8 py-4 bg-white mb-4">
       
       <img src='/Images/logo-png.png' alt='logo' className="w-20 h-20 object-contain rounded-full"></img>
-      
-      <ul className="flex space-x-6 text-gray-700">
-        <li className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">Home</li>
-        <li className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">About</li>
-        <li className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">Services</li>
-        <li className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">Donations</li>
-        <li className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">Events</li>
-        <li className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">How it Works?</li>
+      <ul className="flex space-x-[10vh] text-gray-700">
+      {NavData.map((item,index)=>{
+        return(
+          <Link to={item.path} key={index} className="font-bold text-orange-500 hover:underline text-xl cursor-pointer">{item.label}</Link>
+        )
+      })}
       </ul>
       <div className="flex space-x-4">
         <button className="bg-orange-500 text-white px-4 py-2 rounded-full hover:bg-orange-600">
